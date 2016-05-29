9dd8ad2e32cca39c369477e34b69f9d35e13c499
Appel <Appel@Appel>
2016-05-28 17:07:28 -0700

    5/12/2016 - Initial commit

--- src/cit260/savingThePit/control/UtilitiesControl.java
+++ src/cit260/savingThePit/control/UtilitiesControl.java
@@ -12,41 +12,100 @@ package cit260.savingThePit.control;
 public class UtilitiesControl {
     public String response;
     
-    public String calcTomatoAngleApril(int angle){
+    public int calcTomatoAngleApril(int angle, int howHard){
+        int answer;
+ 
         if (angle > 25) {
-         String response = "Easy there, killer! That's too high!";  
-         return response;
+         answer = -1;
+         //Error Code 1 : "Easy there, killer! That's too high!";
+         return answer;
         }
         
         if (angle < 10) {
-            String response = "That's too low! You throw like Jerry";
-            return response;
+            answer = -2;
+            //Error Code 2 : "That's too low! You're throwing like Jerry";
+            return answer;
+        }
+            
+        if (howHard > 10) {
+            answer = -3;
+            //Error Code 3 : "Lay off the sauce. We want to hit them, not maim them."
+            return answer;
+        } 
+        
+        if (howHard < 1) {
+            answer = -4 ;
+            //Error Code 4 : "Throw it harder! You're an independent woman!"
         }
         
-        if (angle > 9 && angle <26){
-            @SuppressWarnings("LocalVariableHidesMemberVariable")
-            String response = "Nice! You hit him right in the ear. He'll feel that for awhile.";
-            return response;
-        }
+        answer = angle * howHard;
         
-        return response;
+        return answer;
 
     }   
         
     public double calcTablespoonsTom(double tonsOfDirt, double ounces){
         //double VOLUME = 2000;
-        if (tonsOfDirt < 20) {
+        
+        // A negative result will be translated to an error message on the control layer. -1 will indicate the tonsOfDirt number is bad, -2 will indicate that ounces is incorrect.
+        if (tonsOfDirt < 16 || tonsOfDirt > 30) {
             return -1;
         }
         
         if (ounces != 1) {
-            return -1;
+            return -2;
         }
         
         double pounds = tonsOfDirt * 2000;
         double Tbs = pounds / ounces;
         
         return Tbs;
+    }
+    
+    public String calcWafflesAnne(String make){
+        int i = 0;
+        
+        if (i == 1) {
+            String charResponse = "Just one? That’s it? We are still friends, right?";
+            return charResponse;
+        }
+                
+        if (i == 2) {
+            String charResponse = "Keep 'em coming. A woman's gotta eat";
+            return charResponse;
+        }
+                
+        if (i == 3) {
+            String charResponse = "Did you just stuff that fresh one in your face? Do I get any more?";
+            return charResponse;
+        }
+                
+        if (i == 4) {
+            String charResponse = "That's three! That's the appetizers, now for the main course: more!";
+            return charResponse;
+        }
+                
+        if (i == 5) {
+            String charResponse = "Have I mentioned that I love waffles? And that I get really hungry before a 12-hour shift?";
+            return charResponse;
+        }
+                
+        if (i == 6) {
+            String charResponse = "Good job Knope! You make a mean Bisquick waffle.";
+            return charResponse;
+        }
+             
+        if (i == 7) {
+            String charResponse = "Ok seriously, I just ate five. Don't tempt me.";
+            return charResponse;
+        }
+                
+        if (i >= 8) {
+            String charResponse = "Seriously, please stop.";
+            return charResponse;
+        }
+        
+        return response;
     }
     
 }
--- test/cit260/savingThePit/control/UtilitiesControlTest.java
+++ test/cit260/savingThePit/control/UtilitiesControlTest.java
@@ -21,22 +21,114 @@ public class UtilitiesControlTest {
      * Test of calcTomatoAngleApril method, of class UtilitiesControl.
      */
     @Test
-    public void testCalcTomatoAngleApril() {
-        System.out.println("calcTomatoAngleApril");
-        
+    
         /*******************************
          * Test Case #1
          ******************************/
+    public void testCalcTomatoAngleApril() {
+        System.out.println("calcTomatoAngleApril");
+
         System.out.println("\tTest case #1");
         
         int angle = 15;
-        String expResult = "Nice! You hit him right in the ear. He'll feel that for awhile.";
-        // You hit him right in the ear. He'll feel that for awhile"
+        int howHard = 5;
+        int expResult = 75;
+
         UtilitiesControl instance = new UtilitiesControl();
-        String result = instance.calcTomatoAngleApril(angle);
+        int result = instance.calcTomatoAngleApril(angle, howHard);
         assertEquals(expResult, result);
-        System.out.println(expResult);
-        System.out.println(result);
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+        
+        /*******************************
+         * Test Case #2
+         ******************************/
+        
+        System.out.println("\tTest case #2");
+        
+        angle = 15;
+        howHard = 0;
+        expResult = 0;
+ 
+        result = instance.calcTomatoAngleApril(angle, howHard);
+        assertEquals(expResult, result);
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                
+        /*******************************
+         * Test Case #3
+         ******************************/
+        
+        System.out.println("\tTest case #3");
+        
+        angle = 30;
+        howHard = 11;
+        expResult = -1;
+ 
+        result = instance.calcTomatoAngleApril(angle, howHard);
+        assertEquals(expResult, result);
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                
+        /*******************************
+         * Test Case #4
+         ******************************/
+        
+        System.out.println("\tTest case #4");
+        
+        angle = 90;
+        howHard = 3;
+        expResult = -1;
+ 
+        result = instance.calcTomatoAngleApril(angle, howHard);
+        assertEquals(expResult, result);
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                
+        /*******************************
+         * Test Case #5
+         ******************************/
+        
+        System.out.println("\tTest case #5");
+        
+        angle = 25;
+        howHard = 2;
+        expResult = 50;
+ 
+        result = instance.calcTomatoAngleApril(angle, howHard);
+        assertEquals(expResult, result);
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                
+        /*******************************
+         * Test Case #6
+         ******************************/
+        
+        System.out.println("\tTest case #6");
+        
+        angle = 10;
+        howHard = 1;
+        expResult = 10;
+ 
+        result = instance.calcTomatoAngleApril(angle, howHard);
+        assertEquals(expResult, result);
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                
+        /*******************************
+         * Test Case #7
+         ******************************/
+        
+        System.out.println("\tTest case #7");
+        
+        angle = 10;
+        howHard = 10;
+        expResult = 100;
+ 
+        result = instance.calcTomatoAngleApril(angle, howHard);
+        assertEquals(expResult, result);
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
 
     }
 
@@ -46,12 +138,118 @@ public class UtilitiesControlTest {
     @Test
     public void testCalcTablespoonsTom() {
         System.out.println("calcTablespoonsTom");
+        
+        /*******************************
+         * Test Case #1
+         ******************************/
+        
+        System.out.println("\tTest case #1");
+        
         double tonsOfDirt = 20.0;
         double ounces = 1.0;
         UtilitiesControl instance = new UtilitiesControl();
         double expResult = 40000.0;
         double result = instance.calcTablespoonsTom(tonsOfDirt, ounces);
         assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                
+        /*******************************
+         * Test Case #2
+         ******************************/
+        
+        System.out.println("\tTest case #2");
+        
+        tonsOfDirt = 1.0;
+        ounces = 1.0;
+        
+        expResult = -1;
+        result = instance.calcTablespoonsTom(tonsOfDirt, ounces);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                        
+        /*******************************
+         * Test Case #3
+         ******************************/
+        
+        System.out.println("\tTest case #3");
+        
+        tonsOfDirt = 20.0;
+        ounces = 3.0;
+        
+        expResult = -2;
+        result = instance.calcTablespoonsTom(tonsOfDirt, ounces);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                        
+        /*******************************
+         * Test Case #4
+         ******************************/
+        
+        System.out.println("\tTest case #4");
+        
+        tonsOfDirt = 10.0;
+        ounces = 1.0;
+        
+        expResult = -1;
+        result = instance.calcTablespoonsTom(tonsOfDirt, ounces);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                        
+        /*******************************
+         * Test Case #5
+         ******************************/
+        
+        System.out.println("\tTest case #5");
+        
+        tonsOfDirt = 0.0;
+        ounces = 1.0;
+        
+        expResult = -1;
+        result = instance.calcTablespoonsTom(tonsOfDirt, ounces);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                        
+        /*******************************
+         * Test Case #6
+         ******************************/
+        
+        System.out.println("\tTest case #6");
+        
+        tonsOfDirt = 1000.0;
+        ounces = 1.0;
+        
+        expResult = -1;
+        result = instance.calcTablespoonsTom(tonsOfDirt, ounces);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
+                        
+        /*******************************
+         * Test Case #7
+         ******************************/
+        
+        System.out.println("\tTest case #7");
+        
+        tonsOfDirt = 20.0;
+        ounces = -1.0;
+        
+        expResult = -2;
+        result = instance.calcTablespoonsTom(tonsOfDirt, ounces);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\t\tExpected result is: " + expResult);
+        System.out.println("\t\tActual result is: " + result);
 
     }
     
